3a0ee4c7b384d31ad2a706cd78c98de4b024e8c9
Ennio Torre <ennio.torre@cargo-partner.com>
2020-10-12 11:46:32 +0200

    again

--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
 mvn verify -Pit -Pproduction -Dvaadin.proKey=ennioTmad@gmail.com/pro-0a3ab60e-06e2-4c41-b9dc-054060431be6
-# mvn -Pscalability gatling:test -Dgatling.baseUrl=http://backerydemo-dev-rcp-1.apps.osdev.cp.cloud
+# mvn -Pscalability gatling:test -Dgatling.baseUrl=http://backerydemo-dev-rcp-1.apps.osdev.cp.clou
